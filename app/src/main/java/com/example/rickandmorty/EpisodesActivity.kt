57f64be374532fbddd66ae16a9023d04db24c22d
Sergey1404 <93861834+Sergey1404@users.noreply.github.com>
2023-04-17 16:23:05 +0700

    Update EpisodesActivity.kt

--- app/src/main/java/com/example/rickandmorty/EpisodesActivity.kt
+++ app/src/main/java/com/example/rickandmorty/EpisodesActivity.kt
@@ -3,15 +3,15 @@ package com.example.rickandmorty
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.rickandmorty.databinding.ActivityEpisodesBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import timber.log.Timber
 
 class EpisodesActivity: AppCompatActivity() {
     private lateinit var binding: ActivityEpisodesBinding
-    private lateinit var viewModel: EpisodeViewModel
-
     private var episodeList = emptyList<String>()
     private val episodesAdapter = EpisodesAdapter()
 
@@ -20,27 +20,40 @@ class EpisodesActivity: AppCompatActivity() {
         binding = ActivityEpisodesBinding.inflate(layoutInflater)
         setContentView(binding.root)
         Timber.plant(Timber.DebugTree())
-        recyclerViewInit()
+        //Получить список эпизодов и отправить запрос на эти эпизоды
 
         episodeList = intent.getStringArrayListExtra("episodeList")!!
+        Log.d("aaaIntent","get:$episodeList")
 
-        viewModel = ViewModelProvider(this)[EpisodeViewModel::class.java]
-        viewModel.setEpisode(getEpisodeId(episodeList))
-        viewModel.loadEpisodes()
+        recyclerViewInit()
 
-        viewModel.episodeList.observe(this){result->
-            episodesAdapter.submitList(result)
-        }
+        loadEpisodes()
     }
-
-    private fun getEpisodeId(episodeList: List<String>): String {
+//1
+    private fun loadEpisodes()  {
         val url = mutableListOf<String>()
-        for (it in episodeList) {
+        for (it in episodeList){
             val value = it.substringAfterLast("/")
             url.add(value)
         }
-        Log.d("aaa","${url.joinToString(",")}")
-        return url.joinToString(",")
+        val result = url.joinToString(",")
+        Log.d("aaa","$result")
+            //
+
+        MainActivity.api.getEpisodesByCharacter(result)
+            .enqueue(object : Callback<EpisodeResponseNW> {
+            override fun onResponse(call: Call<EpisodeResponseNW>, response: Response<EpisodeResponseNW>) {
+                if (response.isSuccessful) {
+                    episodesAdapter.submitList(response.body())
+                } else {
+                    Log.d("EpisodesActivity", "Response error: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<EpisodeResponseNW>, t: Throwable) {
+                Log.d("EpisodesActivity", "Error: ${t.message}")
+            }
+        })
     }
 
     private fun recyclerViewInit() {
